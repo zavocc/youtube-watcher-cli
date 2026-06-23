fed69cecb8f66868fdb6f0ad2b74224b1e923525
zavocc <buffer.0verfl0w1030@gmail.com>
2026-06-23 18:35:25 +0800

    update build script

--- build.ps1
+++ build.ps1
@@ -1,12 +1,21 @@
 param(
-    [string]$Version = "dev"
+  [string]$Version = "dev",
+  [string]$Os = $(go env GOOS),
+  [string]$Arch = $(go env GOARCH)
 )
 
-$arch = go env GOARCH
-$platform = go env GOOS
+$env:GOOS = $Os
+$env:GOARCH = $Arch
+$env:CGO_ENABLED = 0
+
+if ($env:GOOS -eq "windows") {
+  $EXE = ".exe"
+} else {
+  $EXE = ""
+}
 
 New-Item -Path "outputs" -ItemType Directory -Force
 
 go build `
   -ldflags "-X main.version=$Version" `
-  -o .\outputs\youtube-watcher-cli-$platform-$arch-$Version.exe .
+  -o .\outputs\youtube-watcher-cli-$env:GOOS-$env:GOARCH-$Version$EXE
